830a893f6abba34acc37754d41dc07936c5555fb
reddmonchick <reddmonchick375@gmail.com>
2025-08-01 17:24:41 +0300

    delete comm

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,21 +2,21 @@
 
 set -e
 
-# Ждем доступности БД
+
 echo "Waiting for database..."
 while ! nc -z db ${DB_PORT:-5432}; do
   sleep 0.5
 done
 echo "Database is ready!"
 
-# Устанавливаем приложение (если нужно)
+
 echo "Installing application..."
 poetry install --only-root --no-interaction
 
-# Применяем миграции
+
 echo "Applying database migrations..."
 alembic upgrade head
 
-# Запускаем приложение
+
 echo "Starting application..."
 exec "$@"
